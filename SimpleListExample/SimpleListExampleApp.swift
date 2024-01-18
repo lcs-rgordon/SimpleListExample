385bd6316686f35b19ef0187bbc892eb1162d746
Russell Gordon <rgordon@lcs.on.ca>
2024-01-18 17:20:06 -0500

    Initial Commit

---
+++ SimpleListExample/SimpleListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleListExampleApp.swift
+//  SimpleListExample
+//
+//  Created by Russell Gordon on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct SimpleListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
